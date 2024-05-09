a0f2b45eeaf10e17ac35a23d1857abcad8e5638b
pedro <701.pedro.ferreira@gmail.com>
2024-05-09 22:57:24 +0100

    seasonView html and css advances

--- js/view/seasonsView.js
+++ js/view/seasonsView.js
@@ -1,37 +1,34 @@
-function clear(){
-    const container = document.querySelector('#container');
-    container.innerHTML = '';
+function clear() {
+  const container = document.querySelector('#container');
+  container.innerHTML = '';
 }
 
 async function render(season) {
 
-    const container = document.querySelector('#container');
-    
-    const list = document.createElement('div');
-    list.innerHTML = `<div class="list"></div>`
-    list.style = `display: flex; flex-wrap: wrap ; max-width:95%; gap: 10px; padding: 10px`;
-  
-    function renderSeasons(season) {
-      console.log(season);
-        const item = document.createElement('div');
-        item.innerHTML = ` <a href="#/season/${season.season}">
+  const container = document.querySelector('#container');
+
+  const list = document.createElement('div');
+  list.style = `display: inline; flex-wrap: wrap ; max-width:100%; gap: 10px; padding: 10px`;
+  list.classList.add('list');
+
+  function renderSeasons(season) {
+    const item = document.createElement('div');
+
+    item.innerHTML = `      <a href="#/season/${season.season}">
                             <div class="cardDiv seasonCard">
                              <img class="cardImg" src="${season}" alt="season ${season}">
-                            <div class="cardTextDiv">
                              <h3 class="card-title">${season}</h3>
                              <h5 class="card-title">Drivers Champion: ${season}</h5>
                              <h5 class="card-title">Constructors Champion: ${season.constructorChampName}</h5>
-                             </div>
                            </div>
                            </a>`;
-  
-        list.appendChild(item);
-    }
-  
-    renderSeasons(season);
-  
-    container.appendChild(list);
+
+    list.appendChild(item);
   }
-  
-  export default { clear, render };
-  
+
+  renderSeasons(season);
+
+  container.appendChild(list);
+}
+
+export default { clear, render };
